6bcbf6b598783de50d9c01b45e56f7a0937729dc
Haik Chalian <haikchalian@gmail.com>
2025-04-28 09:10:21 -0700

    Finished up bar chart, added couple sql queries

---
+++ app/dashboard/cashflow-filters.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { useRouter } from 'next/navigation';
+
+type Props = {
+  year: number;
+  yearsRange: number[];
+};
+
+export default function CashFlowFilters({ year, yearsRange }: Props) {
+  const router = useRouter();
+  console.log('');
+  console.log('yearsRange', yearsRange);
+  return (
+    <div>
+      <Select
+        defaultValue={year.toString()}
+        onValueChange={(value) => {
+          router.push(`/dashboard?cfyear=${value}`);
+        }}
+      >
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {yearsRange.map((year) => (
+            <SelectItem key={year} value={year.toString()}>
+              {year}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,24 @@
-import Link from 'next/link'
+import CashFlow from './cashflow';
+import RecentTransactions from './recent-transactions';
 
-export default function DashboardPage() {
-  return <Link href="/dashboard/transactions/new">New Transaction</Link>
+export default async function DashboardPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ cfyear: string }>;
+}) {
+  const today = new Date();
+  const searchParamsValues = await searchParams;
+  let cfYear = Number(searchParamsValues.cfyear ?? today.getFullYear());
+
+  if (isNaN(cfYear)) {
+    cfYear = today.getFullYear();
+  }
+
+  return (
+    <div className="max-w-screen-xl mx-auto py-5">
+      <h1 className="text-4xl font-semibold pb-5">Dashboard</h1>
+      <CashFlow year={cfYear} />
+      <RecentTransactions />
+    </div>
+  );
 }
